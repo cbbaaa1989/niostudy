f886c57635b97a9fa4a399fbfaa61139a60c547e
cbbaaa1989 <yitian_517@163.com>
2016-03-14 00:07:14 +0800

    1

--- src/main/java/pers/fengyitian/server/RequestHandler.java
+++ src/main/java/pers/fengyitian/server/RequestHandler.java
@@ -1,6 +1,24 @@
 package pers.fengyitian.server;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+
 public class RequestHandler implements Handler{
 
-	private Handler
+	private ChannelIO channelIO;
+	
+	private ByteBuffer requestByteBuffer = null;//存放http请求的缓冲区
+	
+	private boolean requestReceived = false;//表示是否已经接收到http请求的所有数据
+	
+	private Request request = null;//表示http请求
+	
+	private Response response = null;//表示http响应
+	
+	public void handle(SelectionKey key) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
