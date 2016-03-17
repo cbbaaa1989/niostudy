defafedb01b3476f6043e06c93a31d03e50174ef
cbbaaa1989 <yitian_517@163.com>
2016-03-17 23:08:34 +0800

    1

--- src/main/java/pers/fengyitian/server2/Start.java
+++ src/main/java/pers/fengyitian/server2/Start.java
@@ -1,98 +1,143 @@
 package pers.fengyitian.server2;
 
+import java.io.BufferedReader;
 import java.io.Console;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
 import java.util.Set;
 
 public class Start {
 
 	
 	public static void main(String[] args){
-		
-		
-		try{
+		new Thread(new Runnable() {
 			
-			ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-			serverSocketChannel.bind(new InetSocketAddress(8080));
-			serverSocketChannel.configureBlocking(false);
-			
-			Selector selector = Selector.open();
-			
-			serverSocketChannel.register(selector, SelectionKey.OP_READ);
-			
-			while(true){
-				int n = selector.selectNow();
-				if(n > 0){
-					Set<SelectionKey> selectedKeys = selector.selectedKeys();
-					while(selectedKeys.iterator().hasNext()){
-						SelectionKey key = selectedKeys.iterator().next();
-						
-						if(key.isAcceptable()){
-							
-							ServerSocketChannel ssc = (ServerSocketChannel)key.channel();
-							SocketChannel socketChannel = ssc.accept();
-							socketChannel.configureBlocking(false);
-							socketChannel.register(selector, SelectionKey.OP_READ);
-						}
-						if(key.isReadable()){
-							SocketChannel socketChannel = (SocketChannel)key.channel();
-							ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-							
-							byteBuffer.flip();
-							
-							
-							 while(read(socketChannel, byteBuffer, selector) > 0){
-								while(byteBuffer.hasRemaining()){
-									char c = (char)byteBuffer.get();
-									if(c == '\n'){
-//										int position = byteBuffer.position();
-//										if(byteBuffer.get(position - 3) == '\r'
-//										&& byteBuffer.get(position - 2) == '\n'
-//										&& byteBuffer.get(position - 1) == '\r'){
-//											key.interestOps() & ~ SelectionKey.OP_READ;
-//										}
-										socketChannel.register(selector, key.interestOps() | ~ SelectionKey.OP_READ);
-										
+			@Override
+			public void run() {
+				try{
+					
+					ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+					serverSocketChannel.bind(new InetSocketAddress(8080));
+					serverSocketChannel.configureBlocking(false);
+					
+					Selector selector = Selector.open();
+					
+					serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+					
+					while(true){
+						//System.out.println("server start select");
+						int n = selector.select();
+						//System.out.println("server select number : " + n);
+						if(n > 0){
+							System.out.println("server select number : " + n);
+							Set<SelectionKey> selectedKeys = selector.selectedKeys();
+							Iterator< SelectionKey> it = selectedKeys.iterator();
+							while(it.hasNext()){
+								SelectionKey key = it.next();
+								
+								if(key.isAcceptable()){
+									
+									ServerSocketChannel ssc = (ServerSocketChannel)key.channel();
+									SocketChannel socketChannel = ssc.accept();
+									if(socketChannel != null){
+										System.out.println("server accept one socket : " + 
+									"port : " + socketChannel.socket().getPort());
+										socketChannel.configureBlocking(false);
+										socketChannel.register(selector, SelectionKey.OP_READ);
+										System.out.println("???????????");
 									}
 								}
-								
-								if(byteBuffer.limit() == byteBuffer.capacity()){
+								if(key.isReadable()){
+									System.out.println("server start read ....");
+									SocketChannel socketChannel = (SocketChannel)key.channel();
+									ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
 									
-									ByteBuffer.allocate(byteBuffer.capacity() * 2).put(byteBuffer);
-								}
-							}
-							 
-							 
-							socketChannel.write(byteBuffer);
+									
+									
+									
+									 while(read(socketChannel, byteBuffer, selector) > 0){
+										 System.out.println("read something .....");
+										while(byteBuffer.hasRemaining()){
+											//char c = (char)byteBuffer.get();
+											//if(c == 'q'){
 
+												//socketChannel.register(selector, key.interestOps() | ~ SelectionKey.OP_READ);
+												System.out.println("server start to write .....");
+												socketChannel.write(byteBuffer);
+											//}
+										}
+										
+										if(byteBuffer.limit() == byteBuffer.capacity()){
+											
+											ByteBuffer.allocate(byteBuffer.capacity() * 2).put(byteBuffer);
+										}
+									}
+									
+
+								}
+								
+								//it.remove();
+								System.out.println("end ....");
+							}
 						}
 					}
+					
+				}catch(Exception e){
+					e.printStackTrace();
 				}
 			}
-			
-		}catch(IOException ioe){
-			
-		}
+		}).start();
 		
-		
-		Console console = System.console();
-		if(console != null){
-			PrintWriter printWriter = console.writer();
-			printWriter.write("input:");  
-			console.flush(); 
+		new Thread(new Runnable() {
 			
-			while(true){
-				String str= console.readLine();
+			@Override
+			public void run() {
+				try {
+					SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress("localhost",8080));
+					ByteBuffer readBuf = ByteBuffer.allocate(1024);
+					BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)) ;
+					while(true){
+						try {
+							String str = reader.readLine();
+							System.out.println(str.getBytes());
+							System.out.println("system in read : " + str);
+							if(str.equals("exit")){
+								return ;
+							}
+							long startWrite = System.currentTimeMillis();
+							int writen = socketChannel.write(ByteBuffer.wrap(str.getBytes()));
+							long endWrite = System.currentTimeMillis();
+							System.out.println("client write time : " + (endWrite - startWrite) + 
+									" .write byte number : " + writen);
+							readBuf.clear();
+							socketChannel.read(readBuf);
+							System.out.println("client read time : " + (System.currentTimeMillis() - endWrite));
+							System.out.println(readBuf);
+							readBuf.flip();
+							
+							System.out.println("read from server is : " + new String(readBuf.array()));
+							
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						
+					}
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
 				
 			}
-		}
+		}).start();
+		
 		
 	}
 	
